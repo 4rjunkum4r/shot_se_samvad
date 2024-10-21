0933335871b3add3cd80dc583448147186937bf2
Arjun Kumar <kumararjundamai1999@gmail.com>
2024-10-21 15:28:19 +0530

    pageNotfound file modification

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import './pageNotFound.css'; // Import Page Not Found styles
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './pageNotFound.css';
 
 const PageNotFound = () => {
   return (
     <div className="page-not-found">
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <Link to="/" className="btn btn-primary">Go Back Home</Link>
+      <div className="page-not-found-content">
+        <h1>Page Not Found</h1>
+        <p>Sorry, the page you are looking for doesn't exist.</p>
+        <NavLink to="/" className="go-back-button">
+          Go Back to Home
+        </NavLink>
+      </div>
     </div>
   );
 };
